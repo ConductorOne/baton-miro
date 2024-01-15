95e745c336e9da35f81a231082c19049e7054373
Vlada Nevyhosteny <vladimir.nevyhosteny@applifting.cz>
2024-01-15 16:40:51 +0100

    feat: team roles sync

--- pkg/connector/teams.go
+++ pkg/connector/teams.go
@@ -2,11 +2,14 @@ package connector
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/conductorone/baton-miro/pkg/miro"
 	v2 "github.com/conductorone/baton-sdk/pb/c1/connector/v2"
 	"github.com/conductorone/baton-sdk/pkg/annotations"
 	"github.com/conductorone/baton-sdk/pkg/pagination"
+	ent "github.com/conductorone/baton-sdk/pkg/types/entitlement"
+	grant "github.com/conductorone/baton-sdk/pkg/types/grant"
 	rs "github.com/conductorone/baton-sdk/pkg/types/resource"
 )
 
@@ -14,6 +17,20 @@ type teamBuilder struct {
 	resourceType   *v2.ResourceType
 	client         *miro.Client
 	organizationId string
+}
+
+const (
+	nonTeamTeamRole   = "non_team"
+	memberTeamRole    = "member"
+	adminTeamRole     = "admin"
+	teamGuestTeamRole = "team_guest"
+)
+
+var teamRoles = []string{
+	nonTeamTeamRole,
+	memberTeamRole,
+	adminTeamRole,
+	teamGuestTeamRole,
 }
 
 func (o *teamBuilder) ResourceType(ctx context.Context) *v2.ResourceType {
@@ -79,10 +96,61 @@ func (g *teamBuilder) List(ctx context.Context, _ *v2.ResourceId, pagination *pa
 }
 
 func (o *teamBuilder) Entitlements(_ context.Context, resource *v2.Resource, _ *pagination.Token) ([]*v2.Entitlement, string, annotations.Annotations, error) {
-	return nil, "", nil, nil
+	var rv []*v2.Entitlement
+
+	for _, role := range teamRoles {
+		assigmentOptions := []ent.EntitlementOption{
+			ent.WithGrantableTo(userResourceType),
+			ent.WithDescription(fmt.Sprintf("Has %s team role", resource.DisplayName)),
+			ent.WithDisplayName(fmt.Sprintf("%s team role %s", resource.DisplayName, role)),
+		}
+
+		entitlement := ent.NewAssignmentEntitlement(resource, role, assigmentOptions...)
+		rv = append(rv, entitlement)
+	}
+
+	return rv, "", nil, nil
 }
 
 // Grants always returns an empty slice for users since they don't have any entitlements.
-func (o *teamBuilder) Grants(ctx context.Context, resource *v2.Resource, pToken *pagination.Token) ([]*v2.Grant, string, annotations.Annotations, error) {
-	return nil, "", nil, nil
+func (o *teamBuilder) Grants(ctx context.Context, resource *v2.Resource, pagination *pagination.Token) ([]*v2.Grant, string, annotations.Annotations, error) {
+	bag, cursor, err := parsePageToken(pagination.Token, &v2.ResourceId{ResourceType: o.resourceType.Id})
+	if err != nil {
+		return nil, "", nil, wrapError(err, "failed to parse page token")
+	}
+
+	response, _, err := o.client.GetTeamMembers(ctx, o.organizationId, resource.Id.Resource, cursor, resourcePageSize)
+	if err != nil {
+		return nil, "", nil, wrapError(err, "failed to get team members")
+	}
+	var grants []*v2.Grant
+	for _, member := range response.Data {
+		if !contains(teamRoles, member.Role) {
+			return nil, "", nil, wrapError(nil, "user does not have a valid team role")
+		}
+
+		user, _, err := o.client.GetOrganizationMember(ctx, o.organizationId, member.Id)
+		if err != nil {
+			return nil, "", nil, wrapError(err, "failed to get user")
+		}
+
+		userResource, err := userResource(ctx, user)
+		if err != nil {
+			return nil, "", nil, wrapError(err, "failed to create user resource")
+		}
+
+		g := grant.NewGrant(resource, member.Role, userResource.Id)
+		grants = append(grants, g)
+	}
+
+	if response.Cursor == "" {
+		return grants, "", nil, nil
+	}
+
+	nextCursor, err := handleNextPage(bag, response.Cursor)
+	if err != nil {
+		return nil, "", nil, wrapError(err, "failed to create next page cursor")
+	}
+
+	return grants, nextCursor, nil, nil
 }
--- pkg/miro/team.go
+++ pkg/miro/team.go
@@ -18,6 +18,16 @@ type (
 		Cursor string `json:"cursor"`
 		Data   []Team `json:"data"`
 	}
+	TeamMember struct {
+		Id   string `json:"id"`
+		Role string `json:"role"`
+	}
+	GetTeamMembersResponse struct {
+		Limit  int32        `json:"limit"`
+		Size   int32        `json:"size"`
+		Cursor string       `json:"cursor"`
+		Data   []TeamMember `json:"data"`
+	}
 )
 
 func (c *Client) GetTeams(ctx context.Context, organizationId, cursor string, limit int32, query ...queryFunction) (*GetTeamsResponse, *http.Response, error) {
@@ -38,6 +48,26 @@ func (c *Client) GetTeams(ctx context.Context, organizationId, cursor string, li
 	}
 
 	return teams, resp, nil
+}
+
+func (c *Client) GetTeamMembers(ctx context.Context, organizationId, teamId, cursor string, limit int32, query ...queryFunction) (*GetTeamMembersResponse, *http.Response, error) {
+	url := fmt.Sprintf("%s/v2/orgs/%s/teams/%s/member", c.baseUrl, organizationId, teamId)
+
+	req, err := c.newRequestWithDefaultHeaders(ctx, http.MethodGet, url)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	query = append(query, WithCursor(cursor), WithLimit(limit))
+	addQueryParams(req, query...)
+
+	teamMembers := new(GetTeamMembersResponse)
+	resp, err := c.do(req, teamMembers)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return teamMembers, resp, nil
 }
 
 func (c *Client) GetTeam(ctx context.Context, organizationId, teamId string) (*Team, *http.Response, error) {
