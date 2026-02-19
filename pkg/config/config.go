0301f9d1479737ea350a8ff91599daaeda48acbb
Robert Chiniquy <rchiniquy@yahoo.com>
2026-02-18 16:05:10 -0800

    Add ExportTargetCLIOnly to base-url field
    Ref: https://github.com/ConductorOne/baton-prismhr/pull/13#discussion_r2790908601

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -23,6 +23,7 @@ var (
 		field.WithDisplayName("Base URL"),
 		field.WithDescription("Override the Miro API URL (for testing or enterprise deployments)"),
 		field.WithHidden(true),
+		field.WithExportTarget(field.ExportTargetCLIOnly),
 	)
 	ConfigurationFields = []field.SchemaField{MiroAccessToken, MiroScimAccessToken, BaseURLField}
 )
